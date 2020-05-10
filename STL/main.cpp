3e1b0db566df157cde6a61eebc891d22eb9881c5
lixuhui123 <2871726754@qq.com>
2020-05-10 22:28:34 +0800

    一些更新

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,16 +1,21 @@
 #if 1
-
+#include <string>
 #include "string.h"
+#include <stdio.h>
 int main()
 {
-	String s1 = "lixu97345646446";
+	/*String s1 = "lixu97345646446";
 	String s2;
 	s2 = s1;
 	String s3(5, 'v');
 	s2.reverse();
 	s3.insert(3,'p');
 	s3.earse(3);
-	cout << s3;
+	cout << s3;*/
+
+	string str = "123";
+	cout << *(&str) <<" "<<&str[0]<<  endl;
+	//printf("%p", &str[0]);
 	system("pause");
 	return 0;
 }
